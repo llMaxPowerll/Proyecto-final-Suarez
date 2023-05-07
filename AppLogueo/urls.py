b93363207f8a7bf6be69256eb55fb9095d3b3fdd
unknown <yoquierofafa@gmail.com>
2023-05-06 22:26:50 -0300

    revisar, solucionado pagina error

--- AppLogueo/urls.py
+++ AppLogueo/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(template_name='AppLogueo/logout.html'), name='logout'),
     path('edit-usuario/', edit_usuario, name='edit-usuario'), #raro
     path('add-avatar/', add_avatar, name='add-avatar'),
-
+    path('accounts/login/', login_request, name='login'),
 
 ]#Listo?
