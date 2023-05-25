72102d70f6a149e69d6029142d924a469f3ffbb8
Chona <yoquierofafa@gmail.com>
2023-05-25 17:55:40 -0300

    creando appmensajes

---
+++ AppMensajes/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from AppMensajes.models import *
+from AppMensajes.forms import *
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
+
+# Create your views here.
